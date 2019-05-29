02b826b6426427815b8a77c16de6740f493869e6
Artem Khodyush <pakafpub@gmail.com>
2019-05-29 09:49:12 -0700

    proper typings for self in service workers is an open issue
    https://github.com/Microsoft/TypeScript/issues/14877#issuecomment-402743582
    
    the workaround is to turn service-worker/index.ts into a module so
    it has its own scope, then you can redeclare "global" variables with
    types that you want

--- src/service-worker/index.ts
+++ src/service-worker/index.ts
@@ -1,6 +1,8 @@
 // This file must have worker types, but not DOM types.
 // The global should be that of a service worker.
-console.log(clients);
+export default null;
+
+const {clients, addEventListener} = self as ServiceWorkerGlobalScope;
 
 addEventListener('fetch', (event) => {
   console.log(event.request);
